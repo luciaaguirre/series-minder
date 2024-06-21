b7745b1ddb45068f6cf1e829a2257bebc49cf0e7
lucia aguirre <guerrolucia@gmail.com>
2024-06-21 10:12:08 +0200

    feat: adds angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 
 import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AuthModule, PagesModule],
+  imports: [BrowserModule, AuthModule, PagesModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
