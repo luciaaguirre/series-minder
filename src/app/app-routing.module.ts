201c78768d089d5b930b9896f61678a6f9e66ffe
lucia aguirre <guerrolucia@gmail.com>
2024-06-26 11:50:18 +0200

    feat: adds app.routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule],
+})
+export class AppRoutingModule {}
