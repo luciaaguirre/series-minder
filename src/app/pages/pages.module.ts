cf210bb42ae6c93aaec567f0ef7b4f64f37def62
lucia aguirre <guerrolucia@gmail.com>
2024-06-27 10:55:30 +0200

    feat: adds auth guard

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SeriesDetailComponent } from './series-detail/series-detail.component';
 import { SeriesListComponent } from './series-list/series-list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'series-list',
     component: SeriesListComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'series-detail/:id',
     component: SeriesDetailComponent,
+    canActivate: [authGuard],
   },
 ];
 
