a9d3723545c1f11406661ad9f2b4ffa532861d00
lucia aguirre <guerrolucia@gmail.com>
2024-07-05 10:21:23 +0200

    feat: adds constructor to RegisterComponent

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register-component',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor() {
+    this.registerForm = new FormGroup({
+      texto: new FormControl(),
+      email: new FormControl(),
+      password: new FormControl(),
+    });
+  }
+}
