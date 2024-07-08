37814017eb55a7eaed655d26f8822bcabe144073
lucia aguirre <guerrolucia@gmail.com>
2024-07-08 10:55:46 +0200

    feat: adds angular material to pages module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SeriesDetailComponent } from './series-detail/series-detail.component';
 import { SeriesListComponent } from './series-list/series-list.component';
-
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [SeriesDetailComponent, SeriesListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatListModule, MatButtonModule, MatIconModule],
   exports: [SeriesDetailComponent, SeriesListComponent],
 })
 export class PagesModule {}
