7bac69cbed4dbf1c30d8495a84550311dabcd1ba
lucia aguirre <guerrolucia@gmail.com>
2024-07-13 09:53:25 +0200

    feat: unsiscribes

--- src/app/pages/series-list/series-list.component.ts
+++ src/app/pages/series-list/series-list.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { SeriesService } from 'src/app/services/series.service';
 import { SeriesModel } from 'src/app/models/series.model';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-series-list',
   templateUrl: './series-list.component.html',
   styleUrls: ['./series-list.component.scss'],
 })
-export class SeriesListComponent implements OnInit {
+export class SeriesListComponent implements OnInit, OnDestroy {
   private seriesService: SeriesService = inject(SeriesService);
-  private seriesList: SeriesModel[] = [];
+  private destroy$: Subject<void> = new Subject();
+  seriesList: SeriesModel[] = [];
 
   ngOnInit(): void {
-    this.seriesService.getAllSeries();
+    this.seriesService
+      .getAllSeries()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (value: SeriesModel[]) => {
+          this.seriesList = value;
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
